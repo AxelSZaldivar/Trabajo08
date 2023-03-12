61b23cb3dcdba1af6d89b9208ced88b5db12a6f9
AxelZaldivar <axel.saldivar5340@alumnos.udg.mx>
2023-03-12 12:32:46 -0600

    Initial commit

---
+++ gato-react/src/components/Next/Next.js
@@ -0,0 +1,20 @@
+import Square from "../Square/Square";
+import './Next.css';
+import classNames from 'classnames';
+
+const Next = ({turn}) =>{
+    var txt = "Turno de: "
+    if(!turn){
+        turn = 'end';
+        txt = "Fin de la partida";
+    }
+
+    return(
+        <div className="next">
+            <h1>{txt}</h1>
+            <Square value={turn} size={1}/>
+        </div>
+    );
+}
+
+export default Next;
